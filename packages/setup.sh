071b1e30b148e24e30e4f9e0759f010d21d9eca9
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-06-07 14:18:00 +0100

    Update setup.sh

--- packages/setup.sh
+++ packages/setup.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
-[ ! -f "/usr/bin/git" ] && sudo pacman -S git --needed --noconfirm
+[ -z "${username}" ] && read -p ":: Type your username (e.g. foo): " username
+[ -z "${user_password}" ] && read -p ":: Type a password for ${username}: " user_password
+
+echo ":: Creating user ${username}..."
+useradd -m -s /bin/zsh "${username}"
+
+echo ":: Setting up password for ${username}..."
+echo "${username}:${user_password}" | chpasswd
+
+export username="${username}"
+
+[ ! -x "/usr/bin/git" ] && sudo pacman -S git --needed --noconfirm
 
 [ ! -d "packages/yay" ] && git -C "packages" clone https://aur.archlinux.org/yay.git && \
-	cd "packages/yay" && makepkg -csi --needed --noconfirm && cd ..
+	cd "packages/yay" && sudo -u ${username} makepkg -csi --needed --noconfirm && cd ..
 
 [ -d "packages/proton-ge-custom-updater" ] && \
-	cd "packages/proton-ge-custom-updater" && makepkg -csi --needed --noconfirm && cd ..
+	cd "packages/proton-ge-custom-updater" && \
+	sudo -u ${username} makepkg -csi --needed --noconfirm && cd ..
 
-[ -f "packages/pkglist" ] && cd "packages"; \
-	yay -Syu --needed --noconfirm - < pkglist
+[ -f "packages/pkglist" ] && \
+	sudo -u ${username} yay -Syu --needed --noconfirm - < packages/pkglist
