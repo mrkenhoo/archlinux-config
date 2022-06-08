b4872bb7374e14fa0278531e246898a77ae3f236
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-06-08 11:42:59 +0100

    Do not create the user automatically

--- packages/setup.sh
+++ packages/setup.sh
@@ -1,24 +1,12 @@
 #!/bin/sh
-
-[ -z "${username}" ] && read -p ":: Type your username (e.g. foo): " username
-[ -z "${user_password}" ] && read -p ":: Type a password for ${username}: " user_password
-
-echo ":: Creating user ${username}..."
-useradd -m -s /bin/zsh "${username}"
-
-echo ":: Setting up password for ${username}..."
-echo "${username}:${user_password}" | chpasswd
-
-export username="${username}"
-
 [ ! -x "/usr/bin/git" ] && sudo pacman -S git --needed --noconfirm
 
 [ ! -d "packages/yay" ] && git -C "packages" clone https://aur.archlinux.org/yay.git && \
-	cd "packages/yay" && sudo -u ${username} makepkg -csi --needed --noconfirm && cd ..
+	cd "packages/yay" && sudo -u `whoami` makepkg -csi --needed --noconfirm && cd ..
 
 [ -d "packages/proton-ge-custom-updater" ] && \
 	cd "packages/proton-ge-custom-updater" && \
-	sudo -u ${username} makepkg -csi --needed --noconfirm && cd ..
+	sudo -u `whoami` makepkg -csi --needed --noconfirm && cd ..
 
 [ -f "packages/pkglist" ] && \
-	sudo -u ${username} yay -Syu --needed --noconfirm - < packages/pkglist
+	sudo -u `whoami` yay -Syu --needed --noconfirm - < packages/pkglist
